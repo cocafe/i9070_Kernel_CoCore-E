41f8a57c280abfc8389450dda52da23a1f05bb70
AdiPat <adithemagnificent@gmail.com>
2013-01-26 18:46:55 +0530

    janice: Get rid of useless Samsung Debug
    Signed-off-by: AdiPat <adithemagnificent@gmail.com>

--- arch/arm/mach-ux500/board-janice.c
+++ arch/arm/mach-ux500/board-janice.c
@@ -52,6 +52,7 @@
 #include <plat/pincfg.h>
 #include <plat/i2c.h>
 #include <plat/ste_dma40.h>
+#include <plat/gpio-nomadik.h>
 
 #include <mach/devices.h>
 #if defined(CONFIG_LIGHT_PROX_GP2A)
@@ -2215,4 +2216,3 @@ MACHINE_START(JANICE, "SAMSUNG JANICE")
 	.init_machine	= janice_init_machine,
 	.restart	= ux500_restart,
 MACHINE_END
-
