94c4c59593b332d2270f060dd05f2fa29aee5d1c
cocafe <1280552236@qq.com>
2013-04-21 15:14:19 +0800

    fs: keep param partition R/W for saving boot params
    [RAW patch:]
    https://github.com/diego-ch/android_kernel_samsung_u8500/commit/fe07ebe725ad91c19d090ec80b8f5d029120504f
    
    Author: Shaaan
    Thanks Shaaan for this idea and patch!!

--- fs/super.c
+++ fs/super.c
@@ -619,10 +619,16 @@ static void do_emergency_remount(struct work_struct *work)
 		spin_unlock(&sb_lock);
 		down_write(&sb->s_umount);
 		if (sb->s_root && sb->s_bdev && !(sb->s_flags & MS_RDONLY)) {
-			/*
-			 * What lock protects sb->s_flags??
-			 */
-			do_remount_sb(sb, MS_RDONLY, NULL, 1);
+			/* FIXME: Samsung param driver needs to save boot params before rebooting */
+                        if ((strcmp(sb->s_id, "mmcblk0p1")) != 0 )	/* param partition */
+			{
+	                        /*
+				 * What lock protects sb->s_flags??
+				 */
+				do_remount_sb(sb, MS_RDONLY, NULL, 1);
+                    	} else {
+        	                 pr_info("super: skipped remount read-only for mmcblk0p1\n");
+			}
 		}
 		up_write(&sb->s_umount);
 		spin_lock(&sb_lock);
