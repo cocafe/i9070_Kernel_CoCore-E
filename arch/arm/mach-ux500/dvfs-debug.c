d42cb1189d660627369c8b89d1082a1baae63c76
cocafe <cocafehj@gmail.com>
2013-09-08 17:08:36 +0800

    dvfs-dbg: update module Signed-off-by: cocafe <cocafehj@gmail.com>

--- arch/arm/mach-ux500/dvfs-debug.c
+++ arch/arm/mach-ux500/dvfs-debug.c
@@ -16,18 +16,60 @@
 
 #define QOS_NAME		"dvfs-dbg"
 
+enum janice_dvfs_list
+{
+	DVFS_APE = 0x00,
+	DVFS_DDR,
+	DVFS_ARM,
+};
+
+enum janice_dvfs_state
+{
+	DEFAULT = 0x00,
+	REQUEST, 
+};
+
+struct janice_dvfs_info
+{
+	int state;
+	int freq;
+};
+
+static struct janice_dvfs_info dvfs_info[] = {
+	[DVFS_APE] = {
+		.state = DEFAULT,
+	},
+	[DVFS_DDR] = {
+		.state = DEFAULT,
+	},
+	[DVFS_ARM] = {
+		.state = DEFAULT,
+		.freq =	0,
+	},
+};
+
+static ssize_t janice_dvfs_ape_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
+{
+	sprintf(buf, "[%s] default\n", dvfs_info[DVFS_APE].state ? " " : "*");
+	sprintf(buf, "%s[%s] max\n", buf, dvfs_info[DVFS_APE].state ? "*" : " ");
+
+	return strlen(buf);
+}
+
 static ssize_t janice_dvfs_ape_store(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count)
 {
 
 	if (!strncmp(buf, "default", 7)) {
-		pr_err("dvfs-dbg: APE OPP -> default\n");
+		pr_err("[DVFS-DBG]: APE OPP -> default\n");
+		dvfs_info[DVFS_APE].state = DEFAULT;
 		prcmu_qos_update_requirement(PRCMU_QOS_APE_OPP, QOS_NAME, PRCMU_QOS_DEFAULT_VALUE);
 
 		return count;
 	}
 
 	if (!strncmp(buf, "max", 3)) {
-		pr_err("dvfs-dbg: APE OPP -> MAX\n");
+		pr_err("[DVFS-DBG]: APE OPP -> MAX\n");
+		dvfs_info[DVFS_APE].state = REQUEST;
 		prcmu_qos_update_requirement(PRCMU_QOS_APE_OPP, QOS_NAME, PRCMU_QOS_APE_OPP_MAX);
 
 		return count;
@@ -36,20 +78,30 @@ static ssize_t janice_dvfs_ape_store(struct kobject *kobj, struct kobj_attribute
 	return count;
 }
 
-static struct kobj_attribute janice_dvfs_ape_interface = __ATTR(ape_opp, 0200, NULL, janice_dvfs_ape_store);
+static struct kobj_attribute janice_dvfs_ape_interface = __ATTR(ape_opp, 0644, janice_dvfs_ape_show, janice_dvfs_ape_store);
+
+static ssize_t janice_dvfs_ddr_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
+{
+	sprintf(buf, "[%s] default\n", dvfs_info[DVFS_DDR].state ? " " : "*");
+	sprintf(buf, "%s[%s] max\n", buf, dvfs_info[DVFS_DDR].state ? "*" : " ");
+
+	return strlen(buf);
+}
 
 static ssize_t janice_dvfs_ddr_store(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count)
 {
 
 	if (!strncmp(buf, "default", 7)) {
-		pr_err("dvfs-dbg: DDR OPP -> default\n");
+		pr_err("[DVFS-DBG]: DDR OPP -> default\n");
+		dvfs_info[DVFS_DDR].state = DEFAULT;
 		prcmu_qos_update_requirement(PRCMU_QOS_DDR_OPP, QOS_NAME, PRCMU_QOS_DEFAULT_VALUE);
 
 		return count;
 	}
 
 	if (!strncmp(buf, "max", 3)) {
-		pr_err("dvfs-dbg: DDR OPP -> MAX\n");
+		pr_err("[DVFS-DBG]: DDR OPP -> MAX\n");
+		dvfs_info[DVFS_DDR].state = REQUEST;
 		prcmu_qos_update_requirement(PRCMU_QOS_DDR_OPP, QOS_NAME, PRCMU_QOS_DDR_OPP_MAX);
 
 		return count;
@@ -58,7 +110,15 @@ static ssize_t janice_dvfs_ddr_store(struct kobject *kobj, struct kobj_attribute
 	return count;
 }
 
-static struct kobj_attribute janice_dvfs_ddr_interface = __ATTR(ddr_opp, 0200, NULL, janice_dvfs_ddr_store);
+static struct kobj_attribute janice_dvfs_ddr_interface = __ATTR(ddr_opp, 0644, janice_dvfs_ddr_show, janice_dvfs_ddr_store);
+
+static ssize_t janice_dvfs_arm_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
+{
+	sprintf(buf, "[%s] default\n", dvfs_info[DVFS_ARM].state ? " " : "*");
+	sprintf(buf, "%s[%s] freq: %d\n", buf, dvfs_info[DVFS_ARM].state ? "*" : " ", dvfs_info[DVFS_ARM].freq);
+
+	return strlen(buf);
+}
 
 static ssize_t janice_dvfs_arm_store(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count)
 {
@@ -66,7 +126,8 @@ static ssize_t janice_dvfs_arm_store(struct kobject *kobj, struct kobj_attribute
 	int freq;
 
 	if (!strncmp(buf, "default", 7)) {
-		pr_err("dvfs-dbg: ARM KHz -> default\n");
+		pr_err("[DVFS-DBG]: ARM KHz -> default\n");
+		dvfs_info[DVFS_ARM].state = DEFAULT;
 		prcmu_qos_update_requirement(PRCMU_QOS_ARM_KHZ, QOS_NAME, PRCMU_QOS_DEFAULT_VALUE);
 
 		return count;
@@ -78,14 +139,16 @@ static ssize_t janice_dvfs_arm_store(struct kobject *kobj, struct kobj_attribute
 		if (freq != 200000 && freq != 400000 && freq != 800000 && freq != 1000000)
 			return count;
 
-		pr_err("dvfs-dbg: ARM KHz -> %d\n", freq);
+		pr_err("[DVFS-DBG]: ARM KHz -> %d\n", freq);
+		dvfs_info[DVFS_ARM].freq = freq;
+		dvfs_info[DVFS_ARM].state = REQUEST;
 		prcmu_qos_update_requirement(PRCMU_QOS_ARM_KHZ, QOS_NAME, freq);
 	}
 	
 	return count;
 }
 
-static struct kobj_attribute janice_dvfs_arm_interface = __ATTR(arm_khz, 0200, NULL, janice_dvfs_arm_store);
+static struct kobj_attribute janice_dvfs_arm_interface = __ATTR(arm_khz, 0644, janice_dvfs_arm_show, janice_dvfs_arm_store);
 
 static struct attribute *janice_dvfs_debug_attrs[] = {
 	&janice_dvfs_ape_interface.attr, 
