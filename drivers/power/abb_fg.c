966399c915c9533e43e217ef613f95869dca79f2
cocafe <cocafehj@gmail.com>
2014-04-30 21:27:34 +0800

    abb-fg: Workarond for fixing laziness on low capacity Signed-off-by: cocafe <cocafehj@gmail.com>

--- drivers/power/abb_fg.c
+++ drivers/power/abb_fg.c
@@ -3182,6 +3182,14 @@ static int ab8500_fg_resume(struct platform_device *pdev)
 		queue_work(di->fg_wq, &di->fg_work);
 	}
 
+	/* 
+	 * FIXME: Workaround to fix laziness on low capacity
+	 */
+	if (di->bat_cap.prev_percent < 5) {
+		ab8500_fg_reinit();
+		pr_info("[ABB-FG] Reinit on low capacity\n");
+	}
+
 	return 0;
 }
 
