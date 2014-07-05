2fdf35c68f22934c8ca3748d453149280e2a1e92
cocafe <cocafehj@gmail.com>
2014-07-05 20:31:34 +0800

    abb-fg: Disable low battery reinit
    Because i want more time on low battery, let it be lazy.
    
    Signed-off-by: cocafe <cocafehj@gmail.com>

--- drivers/power/abb_fg.c
+++ drivers/power/abb_fg.c
@@ -3185,10 +3185,12 @@ static int ab8500_fg_resume(struct platform_device *pdev)
 	/* 
 	 * FIXME: Workaround to fix laziness on low capacity
 	 */
+	/*
 	if (di->bat_cap.prev_percent < 5) {
 		ab8500_fg_reinit();
 		pr_info("[ABB-FG] Reinit on low capacity\n");
 	}
+	*/
 
 	return 0;
 }
