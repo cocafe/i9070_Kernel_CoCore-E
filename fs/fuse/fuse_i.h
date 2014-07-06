40555bb3768e0a9e8db2458756693905009be290
cocafe <cocafehj@gmail.com>
2014-07-06 12:13:50 +0800

    fs/fuse: Sync with LK 3.0.101

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -44,11 +44,6 @@
     doing the mount will be allowed to access the filesystem */
 #define FUSE_ALLOW_OTHER         (1 << 1)
 
-/** If the FUSE_HANDLE_RT_CLASS flag is given,
-    then fuse handle RT class I/O in different request queue  */
-#define FUSE_HANDLE_RT_CLASS   (1 << 2)
-
-
 /** List of active connections */
 extern struct list_head fuse_conn_list;
 
@@ -86,6 +81,9 @@ struct fuse_inode {
 	/** The sticky bit in inode->i_mode may have been removed, so
 	    preserve the original mode */
 	mode_t orig_i_mode;
+
+	/** 64 bit inode number */
+	u64 orig_ino;
 
 	/** Version of last attribute change */
 	u64 attr_version;
@@ -347,10 +345,10 @@ struct fuse_conn {
 	unsigned max_write;
 
 	/** Readers of the connection are waiting on this */
-	wait_queue_head_t waitq[2];
+	wait_queue_head_t waitq;
 
 	/** The list of pending requests */
-	struct list_head pending[2];
+	struct list_head pending;
 
 	/** The list of requests being processed */
 	struct list_head processing;
@@ -380,7 +378,7 @@ struct fuse_conn {
 	struct list_head bg_queue;
 
 	/** Pending interrupts */
-	struct list_head interrupts[2];
+	struct list_head interrupts;
 
 	/** Queue of pending forgets */
 	struct fuse_forget_link forget_list_head;
