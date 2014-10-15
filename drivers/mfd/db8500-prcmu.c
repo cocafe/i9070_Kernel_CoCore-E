e8c4d45e3ae36cce30882c4b90bd50516eec3f80
cocafe <cocafehj@gmail.com>
2014-10-15 21:22:57 +0800

    LiveOPP: Debug config

--- drivers/mfd/db8500-prcmu.c
+++ drivers/mfd/db8500-prcmu.c
@@ -1090,7 +1090,6 @@ static void db8500_prcmu_get_abb_event_buffer(void __iomem **buf)
 #include <linux/mfd/db8500-liveopp.h>
 
 #define LIVEOPP_VER		"2.1"
-/* #define LIVEOPP_DEBUG	1 */
 
 #define NOCHG			0
 #define SET_PLL			1
@@ -1099,7 +1098,7 @@ static void db8500_prcmu_get_abb_event_buffer(void __iomem **buf)
 
 static unsigned int last_arm_idx = 0;
 
-#if LIVEOPP_DEBUG > 1
+#if CONFIG_LIVEOPP_DEBUG > 1
 static int liveopp_start = 0;
 #endif
 
@@ -1528,7 +1527,7 @@ ARM_STEP(arm_step12, 12);
 ARM_STEP(arm_step13, 13);
 ARM_STEP(arm_step14, 14);
 
-#if LIVEOPP_DEBUG > 1
+#if CONFIG_LIVEOPP_DEBUG > 1
 static ssize_t liveopp_start_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)		
 {
 	return sprintf(buf, "%d\n", liveopp_start);
@@ -1542,7 +1541,7 @@ ATTR_RW(liveopp_start);
 #endif
 
 static struct attribute *liveopp_attrs[] = {
-#if LIVEOPP_DEBUG > 1
+#if CONFIG_LIVEOPP_DEBUG > 1
 	&liveopp_start_interface.attr, 
 #endif
 	&version_interface.attr, 
@@ -1766,12 +1765,12 @@ static int arm_set_rate(unsigned long rate)
 	unsigned long frequency = rate / 1000;
 	int i;
 
-#if LIVEOPP_DEBUG > 1
+#if CONFIG_LIVEOPP_DEBUG > 1
 	if (!liveopp_start)
 		return 0;
 #endif
 
-#if LIVEOPP_DEBUG > 0
+#if CONFIG_LIVEOPP_DEBUG > 0
 	pr_info("kHz: %10lu\n", frequency);
 #endif
 
