16d414f660be634b1d2c00456f6515a6fcdc70ce
cocafe <cocafehj@gmail.com>
2014-11-03 21:40:35 +0800

    LiveOPP: Add vbb and avs interfaces
    Also, with some cleanups.
    
    Signed-off-by: cocafe <cocafehj@gmail.com>

--- drivers/mfd/db8500-prcmu.c
+++ drivers/mfd/db8500-prcmu.c
@@ -1087,14 +1087,93 @@ static void db8500_prcmu_get_abb_event_buffer(void __iomem **buf)
 
 #ifdef CONFIG_DB8500_LIVEOPP
 #include <linux/kobject.h>
-#include <linux/mfd/db8500-liveopp.h>
 
 #define LIVEOPP_VER		"2.2"
 
-#define NOCHG			0
-#define SET_PLL			1
-#define SET_EXT			1
-#define SET_VOLT		1
+#define PRCMU_ARMFIX_REG		0x0000
+#define PRCMU_SGACLK_REG		0x0014
+#define PRCMU_PLLSOC0_REG		0x0080
+#define PRCMU_PLLSOC1_REG		0x0084
+#define PRCMU_PLLARM_REG		0x0088
+#define PRCMU_PLLDDR_REG		0x008C
+
+/*
+ * Varm has three voltage selections
+ * But Selection 3 is never used
+ */
+#define AB8500_VARM_SEL1 		0x0B
+#define AB8500_VARM_SEL2 		0x0C
+#define AB8500_VARM_SEL3 		0x0D
+#define AB8500_VBBX_REG 		0x11
+
+/**
+ * struct liveopp_arm_table - Custom frequency and voltage table
+ * @freq_show:		Frequency(KHz) showed in userspace, no effect on real clock
+ * @freq_raw:		Manually calculated frequency(KHz) for showing, no effect on real clock
+ * @pllarm_raw:		Raw register value of PLLARM_FREQ in PRCMU
+ * @varm_raw:		Raw register value of Varm regulator in AB850x
+ * @vbbx_raw:		Raw register value of Vbbp and Vbbn regulator in AB850x
+ * @ddr_opp		minimum DDR_OPP - valid values are 25/default, 50, 100/max
+ * @ape_opp		minimum APE_OPP - valid values are 25/default, 50, 100/max
+ * 			25 is actually APE_50_PARTLY_25_OPP - its OPP_50 with some clocks
+ * 			running at 25% (ACLK and DMACLK)
+ * 			At 50_OPP gpu is running at half speed and uses different voltage
+ * 			(AB8500_REGU_CTRL2/AB8500_VAPESEL2_REG), while at 100_OPP it uses
+ * 			AB8500_REGU_CTRL2/AB8500_VAPESEL1_REG
+ */
+struct liveopp_arm_table
+{
+	u32 	freq_show;
+	u32 	freq_raw;
+	u32 	pllarm_raw;
+	u8 	varm_raw;
+	u8  	vbbx_raw;
+	s8	ddr_opp;
+	s8	ape_opp;
+};
+
+/* 
+ * Varm :
+ * AB8500: in 12.5mV steps 
+ * AB9540/AB8505: in 6.25mV steps
+ */
+#define AB8500_VARM_VSEL_MASK 		0x3f
+#define AB8500_VARM_STEP_UV		12500
+#define AB8505_VARM_STEP_UV		6250
+#define AB8500_VARM_MIN_UV		700000
+#define AB8500_VARM_MAX_UV		1362500
+
+/*
+ * Vbb:
+ * AB8500: in 100mV steps
+ * AB9540/AB8505: in 50mV steps
+ */
+#define AB8500_VBBP_VSEL_MASK		0xf0
+#define AB8500_VBBN_VSEL_MASK		0x0f
+#define AB8500_VBB_STEP_UV		100000
+
+/* PLLARM in 38.4MHz steps */
+#define PLLARM_FREQ_STEPS		38400
+#define PLLARM_MAXOPP			0x0001011A
+#define PLLARM_FREQ100OPP		0x00050168
+
+/* PRCMU AVS registers */
+#define PRCM_AVS_BASE			0x2FC
+#define AVS_VBB_RET			0x0
+#define AVS_VBB_MAX_OPP			0x1
+#define AVS_VBB_100_OPP			0x2
+#define AVS_VBB_50_OPP			0x3
+#define AVS_VARM_MAX_OPP		0x4
+#define AVS_VARM_100_OPP		0x5
+#define AVS_VARM_50_OPP			0x6
+#define AVS_VARM_RET			0x7
+#define AVS_VAPE_100_OPP		0x8
+#define AVS_VAPE_50_OPP			0x9
+#define AVS_VMOD_100_OPP		0xA
+#define AVS_VMOD_50_OPP			0xB
+#define AVS_VSAFE			0xC
+#define AVS_VSAFE_RET			0xD
+#define AVS_SIZE			14
 
 struct mutex liveopp_lock;
 
@@ -1142,6 +1221,19 @@ static const char *armopp_name[] =
 	"ARM_EXTCLK",		/* 0x07 */
 };
 
+static u32 avs_read(u32 reg)
+{
+
+	u8 avs[AVS_SIZE];
+	void __iomem *tcdm_base;
+
+	tcdm_base = __io_address(U8500_PRCMU_TCDM_BASE);
+
+	memcpy_fromio(avs, tcdm_base + PRCM_AVS_BASE, AVS_SIZE);
+
+	return avs[reg];
+}
+
 static int varm_uv(u8 raw)
 {
 	if (raw <= 0x35) {
@@ -1149,6 +1241,58 @@ static int varm_uv(u8 raw)
 	} else {
 		return AB8500_VARM_MAX_UV;
 	}
+}
+
+static int vbbp_uv(u8 raw)
+{
+	int ret;
+	raw &= AB8500_VBBP_VSEL_MASK;
+
+	switch (raw) {
+		case 0x00 ... 0x40:
+			ret = AB8500_VBB_STEP_UV * (raw >> 4);
+			break;
+		case 0x41 ... 0x70:
+			ret =  400000;
+			break;
+		case 0x80 ... 0xb0:
+			ret = -400000;
+			break;
+		case 0xc0 ... 0xf0:
+			ret = -100000 * ((raw >> 4) - 0x0b);
+			break;
+		default:
+			ret = 0;
+			break;
+	}
+
+	return ret;
+}
+
+static int vbbn_uv(u8 raw)
+{
+	int ret;
+	raw &= AB8500_VBBN_VSEL_MASK;
+
+	switch (raw) {
+		case 0x00 ... 0x04:
+			ret = -AB8500_VBB_STEP_UV * raw;
+			break;
+		case 0x05 ... 0x07:
+			ret = -400000;
+			break;
+		case 0x08 ... 0x0c:
+			ret = AB8500_VBB_STEP_UV * (raw - 0x08);
+			break;
+		case 0x0d ... 0x0f:
+			ret =  400000;
+			break;
+		default:
+			ret = 0;
+			break;
+	}
+
+	return ret;
 }
 
 static int pllarm_freq(u32 raw)
@@ -1289,6 +1433,36 @@ static ssize_t arm_varm_show(struct kobject *kobj, struct kobj_attribute *attr, 
 	return sprintf(buf, "%d uV\n", varm_uv(varm));
 }
 ATTR_RO(arm_varm);
+
+static ssize_t arm_vbb_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
+{
+	u8 vbb;
+	prcmu_abb_read(AB8500_REGU_CTRL2, AB8500_VBBX_REG, &vbb, 1);
+
+	return sprintf(buf, "vbbp: %7d uV\nvbbn: %7d uV\n", vbbp_uv(vbb), vbbn_uv(vbb));
+}
+ATTR_RO(arm_vbb);
+
+static ssize_t prcmu_avs_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
+{
+	sprintf(buf,   "VBB_RET        0x%02x\n", avs_read(AVS_VBB_RET));
+	sprintf(buf, "%sVBB_MAX_OPP    0x%02x\n", buf, avs_read(AVS_VBB_MAX_OPP));
+	sprintf(buf, "%sVBB_100_OPP    0x%02x\n", buf, avs_read(AVS_VBB_100_OPP));
+	sprintf(buf, "%sVBB_50_OPP     0x%02x\n", buf, avs_read(AVS_VBB_50_OPP));
+	sprintf(buf, "%sVARM_MAX_OPP   0x%02x\n", buf, avs_read(AVS_VARM_MAX_OPP));
+	sprintf(buf, "%sVARM_100_OPP   0x%02x\n", buf, avs_read(AVS_VARM_100_OPP));
+	sprintf(buf, "%sVARM_50_OPP    0x%02x\n", buf, avs_read(AVS_VARM_50_OPP));
+	sprintf(buf, "%sVARM_RET       0x%02x\n", buf, avs_read(AVS_VARM_RET));
+	sprintf(buf, "%sVAPE_100_OPP   0x%02x\n", buf, avs_read(AVS_VAPE_100_OPP));
+	sprintf(buf, "%sVAPE_50_OPP    0x%02x\n", buf, avs_read(AVS_VAPE_50_OPP));
+	sprintf(buf, "%sVMOD_100_OPP   0x%02x\n", buf, avs_read(AVS_VMOD_100_OPP));
+	sprintf(buf, "%sVMOD_50_OPP    0x%02x\n", buf, avs_read(AVS_VMOD_50_OPP));
+	sprintf(buf, "%sVSAFE          0x%02x\n", buf, avs_read(AVS_VSAFE));
+	sprintf(buf, "%sVSAFE_RET      0x%02x\n", buf, avs_read(AVS_VSAFE_RET));
+
+	return strlen(buf);
+}
+ATTR_RO(prcmu_avs);
 
 static ssize_t arm_step_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf, int _index)
 {
@@ -1436,25 +1610,27 @@ static struct attribute *liveopp_attrs[] = {
 #if CONFIG_LIVEOPP_DEBUG > 1
 	&liveopp_start_interface.attr, 
 #endif
-	&version_interface.attr, 
-	&arm_extclk_interface.attr, 
-	&arm_pllclk_interface.attr, 
+	&version_interface.attr,
+	&prcmu_avs_interface.attr,
+	&arm_extclk_interface.attr,
+	&arm_pllclk_interface.attr,
 	&arm_varm_interface.attr,
-	&arm_step00_interface.attr, 
-	&arm_step01_interface.attr, 
-	&arm_step02_interface.attr, 
-	&arm_step03_interface.attr, 
-	&arm_step04_interface.attr, 
-	&arm_step05_interface.attr, 
-	&arm_step06_interface.attr, 
-	&arm_step07_interface.attr, 
-	&arm_step08_interface.attr, 
-	&arm_step09_interface.attr, 
-	&arm_step10_interface.attr, 
-	&arm_step11_interface.attr, 
-	&arm_step12_interface.attr, 
-	&arm_step13_interface.attr, 
-	&arm_step14_interface.attr, 
+	&arm_vbb_interface.attr,
+	&arm_step00_interface.attr,
+	&arm_step01_interface.attr,
+	&arm_step02_interface.attr,
+	&arm_step03_interface.attr,
+	&arm_step04_interface.attr,
+	&arm_step05_interface.attr,
+	&arm_step06_interface.attr,
+	&arm_step07_interface.attr,
+	&arm_step08_interface.attr,
+	&arm_step09_interface.attr,
+	&arm_step10_interface.attr,
+	&arm_step11_interface.attr,
+	&arm_step12_interface.attr,
+	&arm_step13_interface.attr,
+	&arm_step14_interface.attr,
 	NULL,
 };
 
