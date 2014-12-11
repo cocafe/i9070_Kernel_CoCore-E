eed23a8acff09f3da5f446ae235bec520a29a68c
cocafe <cocafehj@gmail.com>
2014-12-11 12:24:34 +0800

    fsync: Optimize the code of detection
    Signed-off-by: cocafe <cocafehj@gmail.com>

--- fs/sync.c
+++ fs/sync.c
@@ -160,7 +160,7 @@ SYSCALL_DEFINE1(syncfs, int, fd)
 	int ret;
 	int fput_needed;
 
-	if (fsync_mode > 0) {
+	if (unlikely(fsync_mode > 0)) {
 		if (fsync_mode == 1) {
 			return 0;
 		} else if ((fsync_mode == 2) && !is_suspend) {
@@ -197,7 +197,7 @@ int vfs_fsync_range(struct file *file, loff_t start, loff_t end, int datasync)
 	struct address_space *mapping = file->f_mapping;
 	int err, ret;
 
-	if (fsync_mode > 0) {
+	if (unlikely(fsync_mode > 0)) {
 		if (fsync_mode == 1) {
 			return 0;
 		} else if ((fsync_mode == 2) && !is_suspend) {
@@ -237,7 +237,7 @@ EXPORT_SYMBOL(vfs_fsync_range);
  */
 int vfs_fsync(struct file *file, int datasync)
 {
-	if (fsync_mode > 0) {
+	if (unlikely(fsync_mode > 0)) {
 		if (fsync_mode == 1) {
 			return 0;
 		} else if ((fsync_mode == 2) && !is_suspend) {
@@ -254,7 +254,7 @@ static int do_fsync(unsigned int fd, int datasync)
 	struct file *file;
 	int ret = -EBADF;
 
-	if (fsync_mode > 0) {
+	if (unlikely(fsync_mode > 0)) {
 		if (fsync_mode == 1) {
 			return 0;
 		} else if ((fsync_mode == 2) && !is_suspend) {
@@ -272,7 +272,7 @@ static int do_fsync(unsigned int fd, int datasync)
 
 SYSCALL_DEFINE1(fsync, unsigned int, fd)
 {
-	if (fsync_mode > 0) {
+	if (unlikely(fsync_mode > 0)) {
 		if (fsync_mode == 1) {
 			return 0;
 		} else if ((fsync_mode == 2) && !is_suspend) {
@@ -285,7 +285,7 @@ SYSCALL_DEFINE1(fsync, unsigned int, fd)
 
 SYSCALL_DEFINE1(fdatasync, unsigned int, fd)
 {
-	if (fsync_mode > 0) {
+	if (unlikely(fsync_mode > 0)) {
 		if (fsync_mode == 1) {
 			return 0;
 		} else if ((fsync_mode == 2) && !is_suspend) {
@@ -306,7 +306,7 @@ SYSCALL_DEFINE1(fdatasync, unsigned int, fd)
  */
 int generic_write_sync(struct file *file, loff_t pos, loff_t count)
 {
-	if (fsync_mode > 0) {
+	if (unlikely(fsync_mode > 0)) {
 		if (fsync_mode == 1) {
 			return 0;
 		} else if ((fsync_mode == 2) && !is_suspend) {
@@ -378,7 +378,7 @@ SYSCALL_DEFINE(sync_file_range)(int fd, loff_t offset, loff_t nbytes,
 	int fput_needed;
 	umode_t i_mode;
 
-	if (fsync_mode > 0) {
+	if (unlikely(fsync_mode > 0)) {
 		if (fsync_mode == 1) {
 			return 0;
 		} else if ((fsync_mode == 2) && !is_suspend) {
